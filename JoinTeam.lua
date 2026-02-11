f6fc5674a4d196a891e304e529b189570f657cd1
Gdfjffghhjkgghhhh <tnhat08231@gmail.com>
2026-02-12 06:22:06 +0700

    Set team to 'Marines' if player has no team
    Add logic to set player's team to 'Marines' if not already assigned.

--- JoinTeam.lua
+++ JoinTeam.lua
@@ -5,10 +5,8 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lp = Players.LocalPlayer
 
--- chờ Remote chọn team
 local chooseTeam = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("CommF_")
 
--- nếu chưa có team thì chọn Hải Quân
 if not lp.Team or lp.Team.Name ~= "Marines" then
     pcall(function()
         chooseTeam:InvokeServer("SetTeam", "Marines")
